38354b72ea1e1676fe1ed30970376f5d870fbc04
rafaei312654 <97711605+rafaei312654@users.noreply.github.com>
2025-04-11 22:28:15 -0300

    Update auth.js

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -2,6 +2,7 @@ const validUsers = [
   { username: 'rotella', password: 'ferot123', redirect: './img/index.html' },
   { username: 'jow', password: 'dinossauro123', redirect: './aluno.html' },
   { username: 'tadeu', password: 'tadano', redirect: './aluno2.html' },
+  { username: 'Polis', password: 'corno', redirect: './aluno3.html' },
 ];
 
 // Evento que é executado após a página ser carregada
