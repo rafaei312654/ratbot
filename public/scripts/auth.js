ad59f341f21755859ae2c38bf83c2f8481b419cd
rafaei312654 <97711605+rafaei312654@users.noreply.github.com>
2025-04-12 13:18:57 -0300

    Update auth.js

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,40 +1,26 @@
-const validUsers = [
-  { username: 'rotella', password: 'ferot123', redirect: './img/index.html' },
-  { username: 'jow', password: 'dinossauro123', redirect: './aluno.html' },
-  { username: 'tadeu', password: 'tadano', redirect: './aluno2.html' },
-  { username: 'Polis', password: 'Corno321', redirect: './aluno3.html' },
-  { username: 'nat', password: 'ind1o', redirect: './aluno4.html' },
-];
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-// Evento que é executado após a página ser carregada
-window.onload = () => {
-  const loader = document.getElementById('loader');
-  const mainContent = document.getElementById('main-content');
-
-  setTimeout(() => {
-    loader.style.display = 'none'; 
-    mainContent.style.display = 'block';
-  }, 2000); 
-
-
-  const loginForm = document.getElementById('login-form');
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
   const errorMessage = document.getElementById('error-message');
 
-  // Adiciona um evento para tratar o envio do formulário
-  loginForm.addEventListener('submit', (e) => {
-    e.preventDefault(); 
+  try {
+    const response = await fetch('https://login-api.onrender.com/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password })
+    });
 
-    const username = document.getElementById('username').value; // Captura o nome de usuário
-    const password = document.getElementById('password').value; // Captura a senha
+    const data = await response.json();
 
-    // Verifica se o usuário é válido
-    const user = validUsers.find(user => user.username === username && user.password === password);
-    
-    if (user) {
-      errorMessage.textContent = 'Login bem-sucedido!';
-      window.location.href = user.redirect; 
+    if (data.success) {
+      window.location.href = data.redirect;
     } else {
-      errorMessage.textContent = 'Usuário ou senha incorretos.';
+      errorMessage.textContent = data.message;
     }
-  });
-};
+  } catch (error) {
+    errorMessage.textContent = 'Erro de conexão.';
+  }
+});
+
